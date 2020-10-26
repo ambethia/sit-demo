0b0e345869986e83272a0f08c5f1f640a2cc3803
Jason L Perry <jason@suncoast.io>
2020-10-26 00:59:45 -0400

    Update
    A lazy commit.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,12 @@
 class EventsController < ApplicationController
   def show
+    event = Event.find(params[:id])
+            
+    render inertia: 'Event/Show',
+      props: {
+        event: event.as_json(
+          only: [ :id, :name, :description ]
+        )
+      }
   end
 end
